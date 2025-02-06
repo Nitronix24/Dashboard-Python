9d2b146384a001257e6d651995dcd41c483ecdde
VictorHuckel <victor.huckel@student.junia.com>
2025-02-06 10:58:14 +0100

    victor 12

--- DashBoard/DashBoard/backend/dashboard/urls.py
+++ DashBoard/DashBoard/backend/dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from frontend.pages.deskpage import deskpage 
+from frontend.pages.deskpage import deskpage
+from frontend.pages.homepage import homepage 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', deskpage, name='home'),
+    path('', homepage, name='home'),
     path('api/core/', include('APP.core.urls')),
     path('api/desktop/', include('APP.dekstop.urls')),
 ]
