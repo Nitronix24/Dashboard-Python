414858d77eee5b6fec09af1cc421074f15c8b19c
VictorHuckel <victor.huckel@student.junia.com>
2025-02-06 12:08:00 +0100

    no icon on homepage

--- DashBoard/DashBoard/frontend/pages/homepage.py
+++ DashBoard/DashBoard/frontend/pages/homepage.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from backend.APP.core.models import Icon  # Mise à jour de l'import
+ 
 
 def homepage(request):
-    icons = Icon.objects.all()
-    return render(request, 'homepage.html', {'icons': icons})
+    return render(request, 'homepage.html')
+
