7faa2eda1215d6c3059b419dbc8dee8ec7ce49d9
VictorHuckel <victor.huckel@student.junia.com>
2025-02-06 10:26:43 +0100

    victor 6

--- DashBoard/DashBoard/backend/APP/core/models.py
+++ DashBoard/DashBoard/backend/APP/core/models.py
@@ -1,9 +1,23 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
     """Modèle d'utilisateur personnalisé ajoutant un champ bio."""
     bio = models.TextField(blank=True, null=True)
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # nouveau related_name
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set_permissions',  # nouveau related_name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
 
     def __str__(self):
         return self.username
