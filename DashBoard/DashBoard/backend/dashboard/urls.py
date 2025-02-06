730676b5b6dfa5bd7e98825c28faabf1eeb4f915
VictorHuckel <victor.huckel@student.junia.com>
2025-02-06 10:39:01 +0100

    victor 9

--- DashBoard/DashBoard/backend/dashboard/urls.py
+++ DashBoard/DashBoard/backend/dashboard/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, welcome to my Django site!")
+from frontend.pages.deskpage import deskpage 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', deskpage, name='home'),
     path('api/core/', include('APP.core.urls')),
     path('api/desktop/', include('APP.dekstop.urls')),
 ]
