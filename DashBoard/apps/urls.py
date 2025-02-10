054b4b9d8f18ae32c0dfba4ff90101ceb9344d1f
VictorHuckel <victor.huckel@student.junia.com>
2025-02-10 17:13:56 +0100

    victor fin snake

--- DashBoard/apps/urls.py
+++ DashBoard/apps/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 from .views import home, desktop
 
-urlpatterns = [ 
+urlpatterns = [
+    path('', views.terminal, name='terminal'),  # This maps the root URL to the terminal view
     path('desktop/', views.desktop, name='desktop'),
+    path('terminal/', views.terminal, name='terminal'),
+    path('snake/', views.snake, name='snake'),
+    path('agenda/', views.agenda, name='agenda'),  # Added path for agenda
     path('desktop/create_tool/', views.create_tool, name='create_tool')
 ]
--- DashBoard/apps/views.py
+++ DashBoard/apps/views.py
@@ -5,10 +5,15 @@ from .models import ToolBox
 def home (request):
     return render(request, 'apps/home.html')
 
-
+def terminal(request):
+    return render(request, 'apps/terminal.html')
 def desktop(request):
     tools = ToolBox.objects.all()
     return render(request, "apps/desktop.html", {"tools": tools})
+def agenda(request):
+    return render(request, 'apps/agenda.html')
+def snake(request):
+    return render(request, 'apps/snake.html')
 
 def create_tool(request):
     if request.method == "POST":
